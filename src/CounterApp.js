0e658b2a7f31ee9e58ae66b712e57979a57eff42
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-04 17:10:45 -0500

    Usando hooks
    	modificado:     src/CounterApp.js
    	modificado:     src/index.css

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
+
+  //handleAdd
+  const handleAdd = () => {
+    //console.log(e);
+    setCounter(counter + 1);
+    //setCounter(  (c) => c + 1 );  // forma alterna en caso de no poder accesar a counter
+  };
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
