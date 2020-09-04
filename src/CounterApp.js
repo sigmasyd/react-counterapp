5fcbc3014e3c649ce0de9da7df34f0a017a1c365
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-04 14:12:08 -0500

    Se hace component CounterApp
    	nuevo archivo:  src/CounterApp.js
    	modificado:     src/index.js

---
+++ src/CounterApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{value}</h2>
+    </>
+  );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number,
+};
+
+export default CounterApp;
+
+// se puede hacer con shortcode: rafcp
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
 import './index.css';
+import CounterApp from './CounterApp';
 //const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo="Hola mundo" />, divRoot);
+ReactDOM.render(
+  <>
+    <CounterApp value={5} />
+    <PrimeraApp saludo="Hola mundo" />
+  </>,
+  divRoot
+);
 
 //console.log(divRoot);
