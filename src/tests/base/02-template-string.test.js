63f3942486133fa73099785b7fd20d49107f84c4
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-07 17:46:46 -0500

    Pruebas unitarias

---
+++ src/tests/base/02-template-string.test.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { getSaludo } from '../../base/02-template-string';
+
+describe('Pruebas en 02-template-string.js', () => {
+  test('getSaludo debe retornar Hola nombre', () => {
+    const nombre = 'Sigma';
+    const saludo = getSaludo(nombre);
+    //console.log(saludo);
+    expect(saludo).toBe('Hola ' + nombre);
+  });
+  test('getSaludo debe retornar Hola Carlos si no hay argumento nombre', () => {
+    const saludo = getSaludo();
+    expect(saludo).toBe('Hola Carlos');
+  });
+});
