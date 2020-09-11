3ef5e07d3b8dac14e9a29eb0fbd36183bda84678
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-11 16:35:50 -0500

    Pruebas con async-await y fetch

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -1,32 +1,18 @@
-
-
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+export const getImagen = async () => {
+  try {
+    //const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+    const apiKey = 'anyoG6FgTEQ3Fx2KywvgvcZGe1b9Bv1Y';
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
+    const { url } = data.images.original;
 
-    try {
-
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+    return url;
+  } catch (error) {
+    return 'No existe';
+  }
+};
---
+++ src/tests/base/11-async-await.test.js
@@ -0,0 +1,12 @@
+const { getImagen } = require('../../base/11-async-await');
+
+describe('Pruebas con async-await y Fetch', () => {
+  test('debe retornar el url de la imagen', async () => {
+    const url = await getImagen();
+    console.log(url);
+    expect(url.includes('https://')).toBe(true);
+  });
+  /*test('should ', () => {
+    
+  })*/
+});
