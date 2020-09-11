5835ad453b875041cbe7d1f3df60d5e64a77c486
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-11 10:59:10 -0500

    Se envia la prueba de forma apropiada con e parametro callback done

--- src/tests/base/09-promesas.test.js
+++ src/tests/base/09-promesas.test.js
@@ -9,10 +9,12 @@ describe('Pruebas con promesas', () => {
       done();
     });
   });
-  test('debe de obtener un error si el heroe po id no existe', () => {
+  // done es un callback que debe siempre llamarse para indicar a jest que se ha terminado la evaluacion
+  test('debe de obtener un error si el heroe po id no existe', (done) => {
     const id = 10;
     getHeroeByIdAsync(id).catch((error) => {
       expect(error).toBe('No se pudo encontrar el héroe');
+      done();
     });
   });
 });
