b38042be44550045f29011af4648b25879e3f1ff
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-11 18:37:37 -0500

    Se agrega Enzyme para validar componentes de React
    	nuevo archivo:  package-lock.json
    	modificado:     package.json
    	modificado:     src/setupTests.js
    	modificado:     src/tests/PrimeraApp.test.js
    	nuevo archivo:  src/tests/__snapshots__/PrimeraApp.test.js.snap

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
+//import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp />', () => {
-  test('debe mostrar el mensaje "Hola, soy Goku"', () => {
+  /*test('debe mostrar el mensaje "Hola, soy Goku"', () => {
     const saludo = 'Hola, soy Goku';
     const { getByText } = render(<PrimeraApp saludo={saludo} />);
     //wrapper.getByText
     expect(getByText(saludo)).toBeInTheDocument();
+  });*/
+  test('debe de mostrar <PrimeraApp /> correctamente', () => {
+    const saludo = 'Hola, soy Goku';
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
