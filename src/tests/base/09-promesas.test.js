707208be9b6e8a4418f45cd3ff5fa1f84ca4d155
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-11 10:52:40 -0500

    SE utitliza el parametro done para evaluar una promesa correctamente

--- src/tests/base/09-promesas.test.js
+++ src/tests/base/09-promesas.test.js
@@ -1,10 +1,12 @@
 import { getHeroeByIdAsync } from '../../base/09-promesas';
+import heroes from '../../data/heroes';
 
 describe('Pruebas con promesas', () => {
-  test('getHeroeByIdAsync debe retornar un heroe async', () => {
+  test('getHeroeByIdAsync debe retornar un heroe async', (done) => {
     const id = 1;
     getHeroeByIdAsync(id).then((heroe) => {
-      expect(true).toBe(false);
+      expect(heroe).toBe(heroes[0]);
+      done();
     });
   });
 });
