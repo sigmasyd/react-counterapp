b439213df94045f05776af54f728af3e784de04a
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-12 01:20:00 -0500

    Evento de click sobre el boton reset

--- src/CounterApp.test.js
+++ src/CounterApp.test.js
@@ -23,16 +23,19 @@ describe('Prueba sobre counter App', () => {
     const counterText = wrapper.find('h2').text().trim();
     expect(counterText).toBe('11');
   });
-  /*test('debe reset', () => {
-    const btn1 = wrapper.find('button').at(1).simulate('click');
-    //console.log(btn1.html());
-    const counterText = wrapper.find('h2').text().trim();
-    expect(counterText).toBe('10');
-  });*/
   test('debe derementar con el boton -1', () => {
     const btn1 = wrapper.find('button').at(2).simulate('click');
     //console.log(btn1.html());
     const counterText = wrapper.find('h2').text().trim();
     expect(counterText).toBe('9');
+  });
+  test('debe colocar valor por defecto con el btn reset', () => {
+    const wrapper = shallow(<CounterApp value={105} />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    const counterText = wrapper.find('h2').text().trim();
+    console.log(counterText);
+    expect(counterText).toBe('105');
   });
 });
