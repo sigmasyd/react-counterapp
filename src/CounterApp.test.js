68d2dbb542a3af3bec1e03af89e839ab6962f47c
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-09-12 01:12:01 -0500

    Pruebas con los eventos de simulacion de un click

--- src/CounterApp.test.js
+++ src/CounterApp.test.js
@@ -3,14 +3,36 @@ import { shallow } from 'enzyme';
 import CounterApp from './CounterApp';
 
 describe('Prueba sobre counter App', () => {
+  let wrapper = shallow(<CounterApp />); // para mantener la ayuda de los metodos
+
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
   test('debe mostrar <CounterAPP /> correctamente (hacer match con un snapshot) y sus valores por defecto', () => {
-    const wrapper = shallow(<CounterApp />);
     expect(wrapper).toMatchSnapshot();
   });
   test('debe mostrar el valor por defecto de 100', () => {
-    //const valor = 100;
     const wrapper = shallow(<CounterApp value={100} />);
     const valorDefault = wrapper.find('h2').text(); // se puede usar un .trim() para remover espacios si los hay
     expect(valorDefault).toBe(`${100}`);
+  });
+  test('debe incrementar con el boton +1', () => {
+    const btn1 = wrapper.find('button').at(0).simulate('click');
+    //console.log(btn1.html());
+    const counterText = wrapper.find('h2').text().trim();
+    expect(counterText).toBe('11');
+  });
+  /*test('debe reset', () => {
+    const btn1 = wrapper.find('button').at(1).simulate('click');
+    //console.log(btn1.html());
+    const counterText = wrapper.find('h2').text().trim();
+    expect(counterText).toBe('10');
+  });*/
+  test('debe derementar con el boton -1', () => {
+    const btn1 = wrapper.find('button').at(2).simulate('click');
+    //console.log(btn1.html());
+    const counterText = wrapper.find('h2').text().trim();
+    expect(counterText).toBe('9');
   });
 });
